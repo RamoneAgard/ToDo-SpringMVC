37d705e309a9a1e7bb4b32c7bf4d6e112ccaa1c9
RamoneAgard <rhagard@wustl.edu>
2023-02-14 17:27:32 -0600

    create web app initalizer, a demo controller, a view resolver, and a demo view

--- src/main/java/agard/spring/config/WebConfig.java
+++ src/main/java/agard/spring/config/WebConfig.java
@@ -1,11 +1,28 @@
 package agard.spring.config;
 
-        import org.springframework.context.annotation.ComponentScan;
-        import org.springframework.context.annotation.Configuration;
-        import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "agard.spring")
 public class WebConfig {
+
+    // Constants
+    private static final String RESLOVER_PREFIX = "/WEB-INF/view/";
+    private static final String RESLOVER_SUFFIX = ".jsp";
+
+    // Bean Methods
+    @Bean
+    public ViewResolver viewResolver(){
+        UrlBasedViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix(RESLOVER_PREFIX);
+        viewResolver.setSuffix(RESLOVER_SUFFIX);
+        return viewResolver;
+    }
 }
---
+++ src/main/java/agard/spring/controller/DemoController.java
@@ -0,0 +1,24 @@
+package agard.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+
+    //http://loclhost:8080/todo-list/hello
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello";
+    }
+
+    //http://localhost:8080/todo-list/welcome
+    //prefix + name + suffix
+    // /WEB-INF/view/welcome.jsp
+    @GetMapping("welcome")
+    public String welcome(){
+        return "welcome";
+    }
+}
